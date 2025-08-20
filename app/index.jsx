8528690a07ab3aef8b9dbaca4d024e4b413648ee
“Christian09232004 <christianbuhia30@gmail.com>
2025-08-20 09:10:02 +0800

    second commit

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        T U N E S P H E R E  G O A L S
+        T U N E S P H E R E   G O A L S
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
