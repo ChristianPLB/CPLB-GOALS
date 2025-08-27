db03923e6dcd78bfccf8be4efc3a42e4f953460c
“Christain09232004 <christainbuhia30@gmail.com>
2025-08-27 09:46:31 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAO7zzV48Cu2LCK9h6icnXEQ19Ch_1a-kE",
+    authDomain: "cplb-goals.firebaseapp.com",
+    projectId: "cplb-goals",
+    storageBucket: "cplb-goals.firebasestorage.app",
+    messagingSenderId: "3859899895",
+    appId: "1:3859899895:web:f910a70db8342e6bc13af2"
+};
+
+const app = initializeApp(firebaseConfig);
